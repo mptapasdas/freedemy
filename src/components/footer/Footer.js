3c1eff1861b90e917f802b9daf143b234fe58db3
mptapasdas <tapas.das.cer19@itbhu.ac.in>
2021-12-29 18:09:43 +0530

    added home button to navbar

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -2,10 +2,13 @@ import React from "react";
 import "./Footer.css";
 
 const Footer = () => {
+  const githubHref = () => {
+    window.open("https://github.com/mptapasdas/freedemy", "_blank");
+  };
   return (
     <div className='footer-container'>
       <p className='made-by'>A project by Tapas</p>
-      <i className='fab fa-github github-icon'></i>
+      <i className='fab fa-github github-icon' onClick={githubHref}></i>
     </div>
   );
 };
