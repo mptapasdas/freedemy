f55d95d56a0993463da33aed22db0fe3001e6618
mptapasdas <tapas.das.cer19@itbhu.ac.in>
2023-07-09 16:10:48 +0530

    updated backend url

--- src/axios.js
+++ src/axios.js
@@ -1,9 +1,6 @@
 import axios from "axios";
 
-// axios.defaults.baseURL = "https://freedemy.up.railway.app/api/v1/";
-
-axios.defaults.baseURL = "https://freedemy-backend.onrender.com/api/v1"; //error on institute wifi
-// axios.defaults.baseURL = "http://localhost:5000/api/v1";
+axios.defaults.baseURL = "https://freedemy.azurewebsites.net/api/v1";
 
 axios.interceptors.request.use(function (req) {
     const user = localStorage.getItem("user");
