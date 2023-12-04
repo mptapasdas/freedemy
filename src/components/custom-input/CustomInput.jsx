d7a8b27b2d5a0eeb4fba82ebca4c870e3ba87a87
mptapasdas <tapas.das.cer19@itbhu.ac.in>
2023-12-05 01:34:19 +0530

    custom input ui fix

--- src/components/custom-input/CustomInput.jsx
+++ src/components/custom-input/CustomInput.jsx
@@ -1,28 +1,23 @@
 import React from "react";
-import TextField from "@mui/material/TextField";
-// import "./CustomInput.css";
+import "./CustomInput.css";
 
 const CustomInput = (props) => {
-    const { value, hasError, label, type, errorText, onChangeHandler } = props;
-    return (
-        <TextField
-            sx={{ input: { color: "white" } }}
-            InputLabelProps={{
-                style: {
-                    color: "#91c7ba",
-                    fontSize: "0.8rem",
-                },
-            }}
-            className='mt-3'
-            type={type || "text"}
-            value={value}
-            error={hasError}
-            label={label ? label : "label"}
-            helperText={errorText}
-            onChange={onChangeHandler}
-            variant='standard'
-        />
-    );
+  const { value, hasError, label, type, errorText, onChangeHandler } = props;
+  const inputClass = hasError ? "custom-input error-input" : "custom-input";
+
+  return (
+    <div className="mt-3">
+      <input
+        type={type || "text"}
+        id="customInput"
+        className={inputClass}
+        placeholder={label}
+        value={value}
+        onChange={onChangeHandler}
+      />
+      {hasError && <p className="error-message">{errorText}</p>}
+    </div>
+  );
 };
 
 export default CustomInput;
--- src/layouts/sign/operations.jsx
+++ src/layouts/sign/operations.jsx
@@ -1,97 +1,96 @@
 import React, { useState } from "react";
 import CustomInput from "../../components/custom-input/CustomInput";
-
 import { useAuthContext } from "../../context-provider/auth-provider";
-
-import login_image from "../../assets/login_image.png";
+import loginImage from "../../assets/login_image.png";
 
 const Operations = () => {
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-    const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [name, setName] = useState("");
 
-    const { login, register, errorText, authLoading, operation, setOperation } =
-        useAuthContext();
+  const { login, register, errorText, authLoading, operation, setOperation } =
+    useAuthContext();
 
-    const toggleOperation = () => {
-        if (operation === "login") {
-            setOperation("register");
-        } else if (operation === "register") {
-            setOperation("login");
-        }
-    };
-    const authButtonHandler = () => {
-        if (!authLoading) {
-            if (operation === "login") {
-                login(email, password);
-            } else if (operation === "register") {
-                register(name, email, password);
-            }
-        }
-    };
-    return (
-        <div className='container-fluid'>
-            <h1 className='auth-title'>
-                {operation === "login"
-                    ? "Login"
-                    : operation === "register"
-                    ? "Register"
-                    : "Logout"}
-            </h1>
-            <div className='row sign-container justify-content-center align-items-center'>
-                <div className='d-none d-md-block col-md-6'>
-                    <img
-                        src={login_image}
-                        alt='login'
-                        className='login-image'
-                    />
-                </div>
-                <div className='input_container d-flex flex-column justify-content-center align-items-center col-12 col-md-6'>
-                    {operation === "register" && (
-                        <CustomInput
-                            hasError={false}
-                            label='Name'
-                            onChangeHandler={(e) => setName(e.target.value)}
-                            type='text'
-                        />
-                    )}
-                    <CustomInput
-                        hasError={false}
-                        label='Email'
-                        onChangeHandler={(e) => setEmail(e.target.value)}
-                        type='text'
-                    />
-                    <CustomInput
-                        hasError={false}
-                        label='Password'
-                        onChangeHandler={(e) => setPassword(e.target.value)}
-                        type='password'
-                    />
-                    <p className='error-text'>{errorText}</p>
-                    <button
-                        className='operation-button'
-                        disabled={authLoading}
-                        onClick={authButtonHandler}>
-                        {authLoading
-                            ? "Loading..."
-                            : operation === "login"
-                            ? "Login"
-                            : operation === "register"
-                            ? "Register"
-                            : "Logout"}
-                    </button>
-                    <button
-                        className='register-prompt'
-                        disabled={authLoading}
-                        onClick={toggleOperation}>
-                        {operation === "login"
-                            ? "Not a user yet? Register"
-                            : "Already a user? Login"}
-                    </button>
-                </div>
-            </div>
+  const handleToggleOperation = () => {
+    setOperation((prevOperation) => {
+      if (prevOperation === "login") {
+        return "register";
+      }
+      return "login";
+    });
+  };
+
+  const handleAuthButton = () => {
+    if (!authLoading) {
+      if (operation === "login") {
+        login(email, password);
+      } else if (operation === "register") {
+        register(name, email, password);
+      }
+    }
+  };
+
+  return (
+    <div className="container-fluid">
+      <h1 className="auth-title">
+        {operation === "login"
+          ? "Login"
+          : operation === "register"
+          ? "Register"
+          : "Logout"}
+      </h1>
+      <div className="row sign-container justify-content-center align-items-center">
+        <div className="d-none d-md-block col-md-6">
+          <img src={loginImage} alt="login" className="login-image" />
         </div>
-    );
+        <div className="input_container d-flex flex-column justify-content-center align-items-center col-12 col-md-6">
+          {operation === "register" && (
+            <CustomInput
+              hasError={false}
+              label="Name"
+              onChangeHandler={(e) => setName(e.target.value)}
+              type="text"
+            />
+          )}
+          <CustomInput
+            hasError={false}
+            label="Email"
+            onChangeHandler={(e) => setEmail(e.target.value)}
+            type="text"
+          />
+          <CustomInput
+            hasError={false}
+            label="Password"
+            onChangeHandler={(e) => setPassword(e.target.value)}
+            type="password"
+          />
+          <p className="error-text">{errorText}</p>
+          <button
+            className="operation-button"
+            disabled={authLoading}
+            onClick={handleAuthButton}
+          >
+            {authLoading
+              ? "Loading..."
+              : operation === "login"
+              ? "Login"
+              : operation === "register"
+              ? "Register"
+              : "Logout"}
+          </button>
+          <button
+            className="register-prompt"
+            disabled={authLoading}
+            onClick={handleToggleOperation}
+          >
+            {operation === "login"
+              ? "Not a user yet? Register"
+              : "Already a user? Login"}
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Operations;
